083eb048e9dc96b5c3db1cb7927e2f2dfb60f977
masayoshi6 <okumurakiyoshikou@MacBook-Air.local>
2025-07-21 21:25:42 +0900

    コメント追加

--- src/main/java/sekigae/sekigae/SekigaeApplication.java
+++ src/main/java/sekigae/sekigae/SekigaeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SekigaeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SekigaeApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SekigaeApplication.class, args);
+  }
+//あいうえお
 }
