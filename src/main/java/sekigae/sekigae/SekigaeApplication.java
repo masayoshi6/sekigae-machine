456f4166c0138acce7e4a33bd77830d60c0d41ea
masayoshi6 <okumurakiyoshikou@MacBook-Air.local>
2025-07-21 21:13:29 +0900

    firstコミット

---
+++ src/main/java/sekigae/sekigae/SekigaeApplication.java
@@ -0,0 +1,13 @@
+package sekigae.sekigae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SekigaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SekigaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sekigae/sekigae/SekigaeApplicationTests.java
@@ -0,0 +1,13 @@
+package sekigae.sekigae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SekigaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
